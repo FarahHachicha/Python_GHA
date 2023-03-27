85f835a31033f32764f2b9c92a889a34d836dbd0
FarahHachicha <farah.hachicha@ensi-uma.tn>
2023-03-27 11:04:55 +0100

    update pipeline

--- tests/test_shapes_3d.py
+++ tests/test_shapes_3d.py
@@ -5,13 +5,9 @@ from math import pi
 def test_surface_area_of_sphere():
     radius = 2
     sphere = shapes_3d.Sphere(radius)
-
     assert sphere.surface_area == 16 * pi
-
 
 def test_volume_of_sphere():
     radius = 2
     sphere = shapes_3d.Sphere(radius)
-
-    assert sphere.volume == (32 / 3) * pi
-       
+    assert sphere.volume == (32 / 3) * pi    
